9545076aa787a7619787fbe0704c8861603c2c58
Yann Roux <yann.roux@etu.univ-grenoble-alpes.fr>
2024-09-18 08:17:58 +0200

    ajouts fichiers manquant task1

---
+++ task1/src/dev/Broker.java
@@ -0,0 +1,9 @@
+package dev;
+
+public class Broker {
+	public Broker(String name){}
+
+	public Channel accept(int port) {return null;}
+
+	public Channel connect(String name, int port) {return null;}
+}
